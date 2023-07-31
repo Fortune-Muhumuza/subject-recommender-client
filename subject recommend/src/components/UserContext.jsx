978c78eb28132d1176802071420172df4d3f9648
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2023-07-31 19:00:57 +0300

    feat: add user context

--- subject recommend/src/components/UserContext.jsx
+++ subject recommend/src/components/UserContext.jsx
@@ -3,10 +3,14 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const userString = localStorage.getItem("user");
+  const user = JSON.parse(userString);
+
+  // Check if the user is already logged in based on the presence of user data
+  const [isAuthenticated, setIsAuthenticated] = useState(!!user);
 
   return (
-    <UserContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    <UserContext.Provider value={{ isAuthenticated, setIsAuthenticated, user }}>
       {children}
     </UserContext.Provider>
   );
