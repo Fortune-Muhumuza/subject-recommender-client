40a37ab7cda07ed8af69418a37c59f80cefe24c9
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2023-07-31 18:59:33 +0300

    feat: show sidebar links based on role

--- subject recommend/src/components/Sidebar.jsx
+++ subject recommend/src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import { MdHome, MdHistory, MdPerson, MdLock } from "react-icons/md";
+import { UserContext } from "./UserContext";
 
 const Sidebar = () => {
+  const { setIsAuthenticated, user } = useContext(UserContext);
+  const navigate = useNavigate();
+  console.log("user", user);
   return (
     <div className="sidebar">
       <div className="sidebar_header">
@@ -11,40 +15,61 @@ const Sidebar = () => {
       </div>
       <div className="sidebar_links">
         <ul>
+          {user.role === "student" && (
+            <>
+              <li>
+                <Link to="/">
+                  <div className="menu_container">
+                    <MdHome />
+                    <p>Home</p>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/history">
+                  <div className="menu_container">
+                    <MdHistory />
+                    <p>Records</p>
+                  </div>
+                </Link>
+              </li>
+            </>
+          )}
+          {user.role === "teacher" && (
+            <>
+              <li>
+                <Link to="/history">
+                  <div className="menu_container">
+                    <MdHistory />
+                    <p>Records</p>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/profile">
+                  <div className="menu_container">
+                    <MdPerson />
+                    <p>Students</p>
+                  </div>
+                </Link>
+              </li>
+            </>
+          )}
           <li>
-            <Link to="/">
-              <div className="menu_container">
-                <MdHome />
-                <p>Home</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/history">
-              <div className="menu_container">
-              <MdHistory />
-                <p>Records</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile">
-              <div className="menu_container">
-                   <MdPerson />
-                <p>Students</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/login">
-              <div className="menu_container">
-              <MdLock />
+            <Link to="#">
+              <div
+                className="menu_container"
+                onClick={() => {
+                  setIsAuthenticated(false);
+                  // localStorage.removeItem("user");
+                  navigate("/login");
+                }}
+              >
+                <MdLock />
                 <p>Logout</p>
               </div>
             </Link>
           </li>
-          
-       
         </ul>
       </div>
       <div className="sidebar_footer">
