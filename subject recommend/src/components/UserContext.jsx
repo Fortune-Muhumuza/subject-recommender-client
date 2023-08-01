b0e57426429284b289ae632adb4ee324e8f5cd0b
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2023-08-01 11:42:02 +0300

    fixed login issue

--- subject recommend/src/components/UserContext.jsx
+++ subject recommend/src/components/UserContext.jsx
@@ -3,8 +3,13 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+  // Initialize the user object with a default value that includes the role property
+  const defaultUser = {
+    role: "", // Set the default role here (e.g., "student" or "teacher")
+  };
+
   const userString = localStorage.getItem("user");
-  const user = JSON.parse(userString);
+  const user = JSON.parse(userString) || defaultUser;
 
   // Check if the user is already logged in based on the presence of user data
   const [isAuthenticated, setIsAuthenticated] = useState(!!user);
