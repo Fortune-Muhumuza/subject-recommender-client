871a2d81725e2d8dece40c663ee29f64a76e2a0f
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2023-08-02 14:13:35 +0300

    add base url to login

--- subject recommend/src/components/Login.jsx
+++ subject recommend/src/components/Login.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
-import { Form, Input, Button } from "antd";
+import React, { useContext, useEffect, useState } from "react";
+import { Form, Input, Button, Select, Switch } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 import { UserContext } from "./UserContext";
 import axios from "axios";
+import { baseUrl } from "./baseVariables";
 
 const layout = {
   labelCol: { span: 8 },
@@ -16,24 +17,56 @@ const tailLayout = {
 
 const LoginForm = () => {
   const { setIsAuthenticated, setUser } = useContext(UserContext);
-  const navigate = useNavigate()
-  const onFinish = async (values) => {
+  const navigate = useNavigate();
+  const [students, setStudents] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [showAdminUsername, setShowAdminUsername] = useState(false);
+  const [form] = Form.useForm(); // Form instance to control form values
+
+  // Fetch all student names when the component mounts
+  useEffect(() => {
+    fetchAllStudents();
+  }, []);
+
+  const fetchAllStudents = async () => {
     try {
+      const response = await axios.get(`${baseUrl}/api/users/allStudents`);
+      setStudents(response.data);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+    }
+  };
+
+  const handleAdminSwitchChange = (checked) => {
+    setIsAdmin(checked);
+    setShowAdminUsername(checked); // Show/hide the admin username input based on the switch value
+  };
+
+  const onFinish = async (values) => {
+    console.log(values)
+        try {
+      // For admin login, remove the adminUsername field from the form values to avoid any conflicts in the backend
+    
+
       // Send a POST request to the backend server for login
-      const response = await axios.post("https://subjectrec.onrender.com/api/users/login", values);
+      const response = await axios.post(`${baseUrl}/api/users/login`, values);
       const token = response.data.token;
       console.log("Login successful. Token:", token);
       const user = response.data.user;
       // Save the token to local storage
       // localStorage.setItem("token", token);
       localStorage.setItem("user", JSON.stringify(user));
-      if(user.role ==='student'){
-navigate('/')}else{navigate('/history')}
+      if (user.role === "student") {
+        navigate("/");
+      } else {
+        navigate("/history");
+      }
 
       // Update the authentication state to true
       setIsAuthenticated(true);
     } catch (error) {
       console.log("Login failed:", error);
+      alert("Sorry, there was an error. Please try again.");
     }
   };
 
@@ -41,23 +74,61 @@ navigate('/')}else{navigate('/history')}
     console.log("Failed:", errorInfo);
   };
 
-
   return (
     <div className="login_container">
       <h3>Login</h3>
       <Form
         {...layout}
+        form={form}
         name="basic"
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
       >
         <Form.Item
+          label="Admin Login"
+          name="admin"
+          valuePropName="checked" // To work with the Switch component
+        >
+          <Switch onChange={handleAdminSwitchChange} />
+        </Form.Item>
+
+        {/* {isAdmin && showAdminUsername && ( // Show the admin username input conditionally
+          <Form.Item
+            label="Admin Username"
+            name="adminUsername"
+            rules={[
+              {
+                required: true,
+                message: "Please enter the admin username!",
+              },
+            ]}
+          >
+            <Input placeholder="Enter admin username" />
+          </Form.Item>
+        )} */}
+
+        <Form.Item
           label="Username"
           name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
+          rules={[
+            {
+              required: true,
+              message: "Please select your username!",
+            },
+          ]}
         >
-          <Input />
+          {isAdmin ? (
+            <Input placeholder="Enter your username" />
+          ) : (
+            <Select placeholder="Select your username" disabled={isAdmin}>
+              {students.map((student) => (
+                <Select.Option key={student._id} value={student.studentName}>
+                  {student.studentName}
+                </Select.Option>
+              ))}
+            </Select>
+          )}
         </Form.Item>
 
         <Form.Item
