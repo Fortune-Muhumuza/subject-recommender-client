3135f70ca2127e9e28e060a1729409b6d8c83c35
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2023-08-02 22:15:24 +0300

    remove summary from non auth user

--- subject recommend/src/components/Suggestions.jsx
+++ subject recommend/src/components/Suggestions.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useData } from "./DataContext";
+import { UserContext } from "./UserContext";
 
 function Suggestions() {
   const { apiResponse,fetchingAnswer } = useData()
+  const { user } = useContext(UserContext);
   return (
     <div className="response_container">
       {/* <h3>Suggested subjects</h3>
@@ -12,7 +14,7 @@ function Suggestions() {
         <div className="subject_card">Art</div>
         <div className="subject_card">Commerce</div>
       </div> */}
-      <h4>Summary</h4>
+     {user.role === 'student'&& <h4>Summary</h4>}
 
      {!fetchingAnswer? <p>
        {apiResponse?apiResponse:''}
